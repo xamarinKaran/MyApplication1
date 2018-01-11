f280d2209e19b6de42bc7ddf5c43647a312a830e
kariyappa <kariyappa@MacBook.local>
2018-01-11 12:24:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kariyappa/myapplication/second.java
@@ -0,0 +1,24 @@
+package com.example.kariyappa.myapplication;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kariyappa on 11/01/18.
+ */
+
+public class second extends AppCompatActivity {
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
